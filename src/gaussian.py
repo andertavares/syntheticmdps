f9c8e5ed443551dd14c51ad3781904c49ce5e9e5
anderson <andersonrochatavares@gmail.com>
2017-11-12 06:09:21 -0200

    getting to run parallel experiments

--- src/gaussian.py
+++ src/gaussian.py
@@ -8,7 +8,7 @@ import pickle
 
 from agents.learning import LearningAgent
 from env.bandit import Bandit
-from experiments.experiment import Experiment
+from experiments.experiment import Experiment, ParallelExperiment
 from agents.gaussian import *
 from agents.delegator import Delegator
 from util import meeting_point
@@ -26,6 +26,7 @@ p_best_lta = np.zeros((executions, trials))
 expNumber = int(sys.argv[1])
 
 experiments = []
+exp_dict = {}
 
 name = "results_gaussian"
 
@@ -45,33 +46,47 @@ for n_arms in bandit_sizes:
                 )
                 over_actions = Experiment(bandit, learner)
                 over_gaussian_agents = Experiment(bandit, ctrl_gaussian)
-                over_actions.run(trials)
-                over_gaussian_agents.run(trials)
-                #experiments.append(over_actions)
-                #experiments.append(over_gaussian_agents)
+                # over_actions.run(trials)
+                # over_gaussian_agents.run(trials)
+                experiments.append(over_actions)
+                experiments.append(over_gaussian_agents)
 
-                executionRewardsActions[e] = over_actions.rewards
-                executionRewardsGaussian[e] = over_gaussian_agents.rewards
-                p_best_ltd[e] = over_gaussian_agents.p_best
-                p_best_lta[e] = over_actions.p_best
+                experiment_id = '%d/%d/%d/%.2f' % (expNumber, n_arms, team_sz, currentMu)
 
-            meetingPoint = meeting_point(np.mean(executionRewardsActions,0),np.mean(executionRewardsGaussian,0));
+                exp_dict[experiment_id] = (over_actions, over_gaussian_agents)
 
-            plt.figure()
-            plt.plot(np.mean(executionRewardsActions,0),label="Actions")
-            plt.plot(np.mean(executionRewardsGaussian,0),label="Gaussian")
-            plt.plot(np.convolve(np.mean(executionRewardsActions,0), np.ones((100,))/100, mode='valid'));
-            plt.plot(np.convolve(np.mean(executionRewardsGaussian,0), np.ones((100,))/100, mode='valid'));
-            plt.xlabel("Iteration")
-            plt.ylabel("Reward")
-            plt.legend()
-            plt.savefig(name + "/" + str(expNumber)+"/"+str(n_arms)+"/"+str(team_sz)+"/"+str(currentMu)+"/result.pdf");
-            plt.close()
+manager = ParallelExperiment(experiments)
+manager.run(trials)
 
-            pickleFile = open(name + "/" + str(expNumber)+"/"+str(n_arms)+"/"+str(team_sz)+"/"+str(currentMu)+"/results.pickle","wb");
-            pickle.dump([
-                np.mean(executionRewardsActions, 0), np.mean(executionRewardsGaussian,0),
-                np.mean(p_best_lta, 0), np.mean(p_best_ltd, 0),
-                meetingPoint
-            ], pickleFile)
-            pickleFile.close()
+# results = {
+#     'gauss': [r for r in manager.result if str(r.agent) == 'Gaussian'],
+#     'unif': [r for r in manager.result if str(r.agent) == 'Uniform'],
+#     'actions': [over_actions] * len(team_sizes)
+# }
+
+# TODO run experiments, identify them and extract results
+executionRewardsActions[e] = over_actions.rewards
+executionRewardsGaussian[e] = over_gaussian_agents.rewards
+p_best_ltd[e] = over_gaussian_agents.p_best
+p_best_lta[e] = over_actions.p_best
+
+meetingPoint = meeting_point(np.mean(executionRewardsActions,0),np.mean(executionRewardsGaussian,0));
+
+plt.figure()
+plt.plot(np.mean(executionRewardsActions,0),label="Actions")
+plt.plot(np.mean(executionRewardsGaussian,0),label="Gaussian")
+plt.plot(np.convolve(np.mean(executionRewardsActions,0), np.ones((100,))/100, mode='valid'));
+plt.plot(np.convolve(np.mean(executionRewardsGaussian,0), np.ones((100,))/100, mode='valid'));
+plt.xlabel("Iteration")
+plt.ylabel("Reward")
+plt.legend()
+plt.savefig(name + "/" + str(expNumber)+"/"+str(n_arms)+"/"+str(team_sz)+"/"+str(currentMu)+"/result.pdf");
+plt.close()
+
+pickleFile = open(name + "/" + str(expNumber)+"/"+str(n_arms)+"/"+str(team_sz)+"/"+str(currentMu)+"/results.pickle","wb");
+pickle.dump([
+    np.mean(executionRewardsActions, 0), np.mean(executionRewardsGaussian,0),
+    np.mean(p_best_lta, 0), np.mean(p_best_ltd, 0),
+    meetingPoint
+], pickleFile)
+pickleFile.close()
