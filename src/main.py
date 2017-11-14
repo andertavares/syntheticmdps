c0de672d03bf923d9f96490b845cfb65a21de27a
anderson <andersonrochatavares@gmail.com>
2017-11-14 12:09:32 -0200

    processing both experiments in main script

--- src/main.py
+++ src/main.py
@@ -7,12 +7,15 @@ import os
 import sys
 import numpy as np
 import pickle
+import itertools
 
 import config
 from agents.learning import LearningAgent
 from environment.bandit import Bandit
 from experiments.experiment import Experiment, ParallelExperiment
 from agents.gaussian import *
+from agents.uniform import *
+from agents.pruning import *
 from agents.delegator import Delegator
 from util import meeting_point
 
@@ -30,44 +33,74 @@ executions = settings['executions']
 experiments = []
 exp_dict = {}
 
-name = "results_gaussian"
+# values of the prob. distribution of agent generation
+# it vary in nature if we're dealing with gaussian or uniform
+dist_params = settings['upper_bounds']
+
+if settings['ltd_type'] == 'gaussian':
+    dist_params = itertools.product(settings['mus'], settings['sigmas'])
+
+#name = "results_gaussian"
+
+print(settings['output_dir'])
 
 for n_arms in bandit_sizes:
     for team_sz in team_sizes:
-        for currentMu in mus:
-            os.system("mkdir -p " + os.path.join(name, str(n_arms), str(team_sz), '%.2f' % currentMu))
+        for param in dist_params:
+
+            # if experiment is gaussian, param has two values
+            mu_or_upper_bound = param if settings['ltd_type'] == 'uniform' else param[0]
+
+            os.system(
+                "mkdir -p " + os.path.join(
+                    settings['output_dir'], str(n_arms), str(team_sz), '%.2f' % mu_or_upper_bound
+                )
+            )
 
             # identifies groups of experiments by their parameters
-            exp_group_name = '%d/%d/%.2f' % (n_arms, team_sz, currentMu)
+            exp_group_name = '%d/%d/%.2f' % (n_arms, team_sz, mu_or_upper_bound)
             exp_dict[exp_group_name] = {'LtA': [], 'LtD': []}
 
             for e in range(executions):
+                sys.stdout.write(
+                    '\rSetup for %d arms, |X| = %6d, u/mu = %.4f, exec=%6d' %
+                    (n_arms, team_sz, mu_or_upper_bound, e)
+                )
 
                 bandit = Bandit(n_arms, None, 0.25)
 
                 learner = LearningAgent(bandit, alpha=1.0, epsilon=1.0, alpha_decay=0.999, epsilon_decay=0.999)
-                ctrl_gaussian = Delegator(
-                    [GaussianAgentPrune(
-                        bandit, 0.95, mu=currentMu, sigma=sigma
-                    ) for _ in range(team_sz)],
-                    alpha=1.0,
-                    epsilon=1.0, alpha_decay=0.999, epsilon_decay=0.999
-                )
-                experiment_id = '%d/%d/%d/%.2f' % (e, n_arms, team_sz, currentMu)
 
-                over_actions = Experiment(bandit, learner, 'LtA/' + experiment_id)
-                over_gaussian_agents = Experiment(bandit, ctrl_gaussian, 'LtD/' + experiment_id)
+                if settings['ltd_type'] == 'uniform':
+                    controller = Delegator(
+                        [PruningAgentFair2(bandit, 0.95, u=mu_or_upper_bound) for _ in range(team_sz)],
+                        alpha=1.0,
+                        epsilon=1.0, alpha_decay=0.999, epsilon_decay=0.999
+                    )
+                else:
+                    controller = Delegator(
+                        [GaussianAgentPrune(
+                            bandit, 0.95, mu=mu_or_upper_bound,  sigma=param[1]
+                        ) for _ in range(team_sz)],
+                        alpha=1.0,
+                        epsilon=1.0, alpha_decay=0.999, epsilon_decay=0.999
+                    )
+
+                experiment_id = '%d/%d/%d/%.2f' % (e, n_arms, team_sz, mu_or_upper_bound)
+
+                lta_experiment = Experiment(bandit, learner, 'LtA/' + experiment_id)
+                ltd_experiment = Experiment(bandit, controller, 'LtD/' + experiment_id)
                 # over_actions.run(trials)
                 # over_gaussian_agents.run(trials)
-                experiments.append(over_actions)
-                experiments.append(over_gaussian_agents)
+                experiments.append(lta_experiment)
+                experiments.append(ltd_experiment)
 
 
 
-print('Setup finished.')
+print('\nSetup finished.')
 manager = ParallelExperiment(experiments)
 manager.run(trials)
-print('Experiments finished.')
+print('\nExperiments finished.')
 # organize results
 
 for r in manager.result:
@@ -81,7 +114,7 @@ for r in manager.result:
 print('Results organized')
 for exp_group_name, exp_group in exp_dict.items():
     executionRewardsActions = [exp.rewards for exp in exp_group['LtA']]
-    executionRewardsGaussian = [exp.rewards for exp in exp_group['LtD']]
+    executionRewardsLtD = [exp.rewards for exp in exp_group['LtD']]
 
     p_best_lta = [exp.p_best for exp in exp_group['LtA']]
     p_best_ltd = [exp.p_best for exp in exp_group['LtD']]
@@ -92,50 +125,58 @@ for exp_group_name, exp_group in exp_dict.items():
     cumulative_rewards_lta = [exp.cumulative_rewards for exp in exp_group['LtA']]
     cumulative_rewards_ltd = [exp.cumulative_rewards for exp in exp_group['LtD']]
 
-    meetingPoint = meeting_point(np.mean(executionRewardsActions, 0), np.mean(executionRewardsGaussian, 0))
+    meetingPoint = meeting_point(np.mean(executionRewardsActions, 0), np.mean(executionRewardsLtD, 0))
+
+    ltd_name = 'Gaussian' if settings['ltd_type'] == 'gaussian' else 'Uniform'
 
     plt.figure()
     plt.plot(np.mean(executionRewardsActions, 0), label="Actions")
-    plt.plot(np.mean(executionRewardsGaussian, 0), label="Gaussian")
+    plt.plot(np.mean(executionRewardsLtD, 0), label=ltd_name)
     plt.plot(np.convolve(np.mean(executionRewardsActions, 0), np.ones((100,))/100, mode='valid'))
-    plt.plot(np.convolve(np.mean(executionRewardsGaussian, 0), np.ones((100,))/100, mode='valid'))
+    plt.plot(np.convolve(np.mean(executionRewardsLtD, 0), np.ones((100,)) / 100, mode='valid'))
     plt.xlabel("Iteration")
     plt.ylabel("Reward")
     plt.legend()
-    plt.savefig(os.path.join(name, exp_group_name, "reward.pdf"))
+    plt.savefig(os.path.join(settings['output_dir'], exp_group_name, "reward.pdf"))
     plt.close()
 
     plt.figure()
     plt.plot(np.mean(cumulative_rewards_lta, 0), label="Actions")
-    plt.plot(np.mean(cumulative_rewards_ltd, 0), label='Gaussian')
+    plt.plot(np.mean(cumulative_rewards_ltd, 0), label=ltd_name)
     plt.xlabel("Iteration")
     plt.ylabel("Cumulative reward")
     plt.legend()
     # plt.show()  #it does not work
-    plt.savefig(os.path.join(name, exp_group_name, "reward_acc.pdf"))
+    plt.savefig(os.path.join(settings['output_dir'], exp_group_name, "reward_acc.pdf"))
 
     plt.figure()
     plt.plot(np.mean(p_best_lta, 0), label="Actions")
-    plt.plot(np.mean(p_best_ltd, 0), label='Gaussian')
+    plt.plot(np.mean(p_best_ltd, 0), label=ltd_name)
     plt.xlabel("Iteration")
     plt.ylabel("Prob. of best action")
     plt.legend()
     # plt.show()  #it does not work
-    plt.savefig(os.path.join(name, exp_group_name, 'pbest_gaussian.pdf'))
+    plt.savefig(os.path.join(settings['output_dir'], exp_group_name, 'pbest.pdf'))
 
     plt.figure()
     plt.plot(np.mean(times_best_lta, 0), label="Actions")
-    plt.plot(np.mean(times_best_ltd, 0), label='Gaussian')
+    plt.plot(np.mean(times_best_ltd, 0), label=ltd_name)
     plt.xlabel("Iteration")
     plt.ylabel("#times played best action so far")
     plt.legend()
     # plt.show()  #it does not work
-    plt.savefig(os.path.join(name, exp_group_name, "tbest_gaussian.pdf"))
+    plt.savefig(os.path.join(settings['output_dir'], exp_group_name, "tbest.pdf"))
 
-    pickleFile = open(os.path.join(name, exp_group_name, "results.pickle"), "wb")
+    pickleFile = open(os.path.join(settings['output_dir'], exp_group_name, "results.pickle"), "wb")
     pickle.dump([
-        np.mean(executionRewardsActions, 0), np.mean(executionRewardsGaussian, 0),
-        np.mean(p_best_lta, 0), np.mean(p_best_ltd, 0),
+        np.mean(executionRewardsActions, 0),
+        np.mean(executionRewardsLtD, 0),
+        np.mean(p_best_lta, 0),
+        np.mean(p_best_ltd, 0),
+        np.mean(cumulative_rewards_lta, 0),
+        np.mean(cumulative_rewards_ltd, 0),
+        np.mean(times_best_lta, 0),
+        np.mean(times_best_ltd, 0),
         meetingPoint
     ], pickleFile)
     pickleFile.close()
